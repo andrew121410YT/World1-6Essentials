64275d0c54e77fc0c01593db0ac24b6d48325b33
Andrews-Laptop <andrews-laptop@desktop-au3q4kj>
2018-12-08 17:22:51 -0500

    Omg

--- src/main/java/Commands/debug.java
+++ src/main/java/Commands/debug.java
@@ -108,54 +108,56 @@ public class debug implements CommandExecutor {
                             p.sendMessage(Translate.chat("Here's the HashMap for Events.OnJoin.keyDatam: " + keyDataM.get(p.getDisplayName())));
 
                         }
-
-                        //CLEAR ALL ARRAYLIST
-                    } else if (args.length == 1 && (args[0].equalsIgnoreCase("clearallarraylists"))) {
-                        if (!p.hasPermission("command.debugcheckhashmaps.permission")) { // Permission
-                            api.PermissionErrorMessage(p);
-                            return true;
-                        }
-                        api.clearAllArrayLists();
-                        return true;
-
-                        //CLEAR ALL HASHMAPS
-                    } else if (args.length == 1 && (args[0].equalsIgnoreCase("clearallhashmaps"))) {
-                        if (!p.hasPermission("command.debugclearallhashmaps.permission")) { // Permission
-                            api.PermissionErrorMessage(p);
-                            return true;
-                        }
-                        api.clearAllHashMaps();
-                        return true;
-
-                        //CLEAR ALL HASHMAPS WITH THE NAME.
-                    } else if (args.length == 1 && (args[0].equalsIgnoreCase("clearallhashmapswithname"))) {
-                        if (!p.hasPermission("command.debugclearallhashmapswithname.permission")) { // Permission
-                            api.PermissionErrorMessage(p);
-                            return true;
-                        }
-                        api.clearAllHahsMapsWithName(p);
-                        return true;
-
-                        //SQL
-                    } else if (args.length >= 2 && (args[0].equalsIgnoreCase("sql"))) {
-                        if (!p.hasPermission("command.debugsql.permission")) { // Permission
-                            api.PermissionErrorMessage(p);
-                            return true;
-                        }
-                        // String Builder
-                        StringBuilder builder = new StringBuilder();
-                        for (int i = 1; i < args.length; i++) {
-                            builder.append(args[i] + " ");
-                        }
-                        String msg = builder.toString();
-                        mysql.Connect();
-                        mysql.ExecuteCommand(msg);
-                        p.sendMessage(Translate.chat("&4&lYour command has been executed thru SQL."));
-                        p.sendMessage(Translate.chat("&aHere's the command you did &r" + msg));
-                        return true;
-                    } else {
                     }
                 }
+
+                //CLEAR ALL ARRAYLIST
+            } else if (args.length == 1 && (args[0].equalsIgnoreCase("clearallarraylists"))) {
+                if (!p.hasPermission("command.debugcheckhashmaps.permission")) { // Permission
+                    api.PermissionErrorMessage(p);
+                    return true;
+                }
+                api.clearAllArrayLists();
+                p.sendMessage(Translate.chat("&bOK..."));
+                return true;
+
+                //CLEAR ALL HASHMAPS
+            } else if (args.length == 1 && (args[0].equalsIgnoreCase("clearallhashmaps"))) {
+                if (!p.hasPermission("command.debugclearallhashmaps.permission")) { // Permission
+                    api.PermissionErrorMessage(p);
+                    return true;
+                }
+                api.clearAllHashMaps();
+                p.sendMessage(Translate.chat("&bOK..."));
+                return true;
+
+                //CLEAR ALL HASHMAPS WITH THE NAME.
+            } else if (args.length == 1 && (args[0].equalsIgnoreCase("clearallhashmapswithname"))) {
+                if (!p.hasPermission("command.debugclearallhashmapswithname.permission")) { // Permission
+                    api.PermissionErrorMessage(p);
+                    return true;
+                }
+                api.clearAllHahsMapsWithName(p);
+                return true;
+
+                //SQL
+            } else if (args.length >= 2 && (args[0].equalsIgnoreCase("sql"))) {
+                if (!p.hasPermission("command.debugsql.permission")) { // Permission
+                    api.PermissionErrorMessage(p);
+                    return true;
+                }
+                // String Builder
+                StringBuilder builder = new StringBuilder();
+                for (int i = 1; i < args.length; i++) {
+                    builder.append(args[i] + " ");
+                }
+                String msg = builder.toString();
+                mysql.Connect();
+                mysql.ExecuteCommand(msg);
+                p.sendMessage(Translate.chat("&4&lYour command has been executed thru SQL."));
+                p.sendMessage(Translate.chat("&aHere's the command you did &r" + msg));
+                return true;
+            } else {
             }
         }
         return true;
