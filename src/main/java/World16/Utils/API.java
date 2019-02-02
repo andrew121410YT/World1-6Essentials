84978639b05a8514313bf3f6f914f92552dc900c
andrew121410 <andrewcherring@gmail.com>
2019-02-02 11:41:31 -0500

    API UPDATE why not

--- src/main/java/World16/Utils/API.java
+++ src/main/java/World16/Utils/API.java
@@ -47,7 +47,7 @@ public class API {
 
     //finals
     public static final Integer VERSION = 5;
-    public static final String DATE_OF_VERSION = "1/29/2019";
+    public static final String DATE_OF_VERSION = "2/2/2019";
     public static final String PREFIX = "[&9World1-6Ess&r]";
     public static final String USELESS = "" + PREFIX + "->[&bUSELESS&r]";
     public static final String TOO_DAMN_OLD = "Your mc version is too damn old 1.11 up too 1.13.2 please.";
@@ -126,8 +126,20 @@ public class API {
         return GodM.contains(p.getDisplayName());
     }
 
+    public boolean isDebug() {
+        return this.plugin.getConfig().getString("debug").equalsIgnoreCase("true");
+    }
+
     public ArrayList<String> getAfkArrayList() {
-        return Afk1;
+        return afk.Afk;
+    }
+
+    public ArrayList<String> getFlyArrayList(){
+        return fly.Fly;
+    }
+
+    public ArrayList<String> getGodArrayList() {
+        return god.godm;
     }
 
     public void clearArrayListAndHashMaps(Player p) {
@@ -345,10 +357,6 @@ public class API {
         this.configinstance.getshit()
                 .set("Spawn." + this.spawnname1 + ".Player.Data.UUID", p.getUniqueId().toString());
         this.configinstance.saveshit();
-    }
-
-    public boolean isDebug() {
-        return plugin.getConfig().getString("debug").equalsIgnoreCase("true");
     }
 
     public void PermissionErrorMessage(Player p) {
