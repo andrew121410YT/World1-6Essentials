fea507970c08ecafbe64cb19d265fa02802f4c80
Andrew121410 <andrewcherring@gmail.com>
2019-02-11 17:12:33 -0500

    Added a instanceof

--- src/main/java/World16/Events/InventoryClickEvent.java
+++ src/main/java/World16/Events/InventoryClickEvent.java
@@ -2,10 +2,12 @@ package World16.Events;
 
 import World16.CustomInventorys.CustomInventoryManager;
 import World16.Main.Main;
+import World16.Utils.ICustomInventory;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.InventoryHolder;
 
 public class InventoryClickEvent implements Listener {
 
@@ -20,8 +22,9 @@ public class InventoryClickEvent implements Listener {
     @EventHandler
     public void onClick(org.bukkit.event.inventory.InventoryClickEvent event) {
         String inv_name = event.getInventory().getTitle();
+        final InventoryHolder holder = event.getInventory().getHolder();
 
-        if (inv_name.equals(CustomInventoryManager.getTestMenu().getInventoryName()) && (event.getCurrentItem() != null) && (!event.getCurrentItem().getType().equals(Material.AIR) && (event.getClickedInventory() != null))) {
+        if (inv_name.equals(CustomInventoryManager.getTestMenu().getInventoryName()) && (event.getCurrentItem() != null) && (!event.getCurrentItem().getType().equals(Material.AIR) && (event.getClickedInventory() != null) && (holder instanceof ICustomInventory))) {
             event.setCancelled(true);
             CustomInventoryManager.getTestMenu().clicked((Player) event.getWhoClicked(), event.getSlot(), event.getCurrentItem(), event.getInventory());
         } else {
