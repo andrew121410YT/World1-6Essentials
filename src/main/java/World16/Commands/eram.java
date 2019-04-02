e67e72adf97caeafd471ded5f4e8af0521207ae9
andrew121410 <andrewcherring@gmail.com>
2019-04-02 16:30:59 -0400

    Big Update

--- src/main/java/World16/Commands/eram.java
+++ src/main/java/World16/Commands/eram.java
@@ -3,16 +3,11 @@ package World16.Commands;
 import World16.CustomConfigs.CustomConfigManager;
 import World16.Events.PlayerInteractEvent;
 import World16.Main.Main;
-import World16.Objects.RawLocationObject;
 import World16.Storage.OldMySQL;
-import World16.Utils.API;
-import World16.Utils.CustomYmlManager;
-import World16.Utils.KeyAPI;
-import World16.Utils.Translate;
+import World16.Utils.*;
 import World16.test.ERamManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -31,6 +26,11 @@ public class eram implements CommandExecutor {
     private CustomYmlManager shitYml = null;
     private CustomConfigManager customConfigManager;
 
+    //Maps
+    Map<String, Map<String, List<Location>>> aaa = ERamManager.stringRawLocationObjectHashMap;
+    Map<String, Location> latestClickedBlocked = PlayerInteractEvent.latestClickedBlocked;
+    //...
+
     public eram(CustomConfigManager getCustomYml, Main getPlugin) {
         this.shitYml = getCustomYml.getShitYml();
         this.customConfigManager = getCustomYml;
@@ -42,9 +42,6 @@ public class eram implements CommandExecutor {
         this.plugin.getCommand("eram").setExecutor(this);
     }
 
-    Map<String, Map<String, RawLocationObject>> aaa = ERamManager.stringRawLocationObjectHashMap;
-    Map<String, Location> latestClickedBlocked = PlayerInteractEvent.latestClickedBlocked;
-
     private ERamManager eRamManager;
 
     @Override
@@ -53,21 +50,24 @@ public class eram implements CommandExecutor {
             if (args.length == 3 && args[0].equalsIgnoreCase("do") && args[1] != null && args[2] != null) {
                 eRamManager.doIt(args[1], args[2]);
                 return true;
-            }else if(args.length == 8 && args[0].equalsIgnoreCase("copy")){
+            } else if (args.length == 7 && args[0].equalsIgnoreCase("copy")) {
                 //copy
                 String playerName = args[1];
                 UUID uuid = UUID.fromString(args[2]);
                 String saveName = args[3];
-                String letter = args[4];
-                String x = args[5];
-                String y = args[6];
-                String z = args[7];
+                String x = args[4];
+                String y = args[5];
+                String z = args[6];
+
+                if (saveName.startsWith("@")) {
+                    String tagWithoutA = saveName.replace("@", "");
+                    saveName = Tag.getTag(playerName, tagWithoutA);
+                }
 
                 aaa.computeIfAbsent(playerName, k -> new HashMap<>());
-                if (aaa.get(playerName).get(saveName) == null) {
-                    aaa.get(playerName).put(saveName, new RawLocationObject());
-                }
-                aaa.get(playerName).get(saveName).set(letter, new Location(Bukkit.getWorld("world"), Double.valueOf(x), Double.valueOf(y), Double.valueOf(z)));
+                aaa.get(playerName).computeIfAbsent(saveName, k -> new ArrayList<>());
+
+                aaa.get(playerName).get(saveName).add(new Location(Bukkit.getWorld("world"), Double.valueOf(x), Double.valueOf(y), Double.valueOf(z)));
                 eRamManager.saveThingy(playerName, uuid, saveName);
             }
             sender.sendMessage("Only Players Can Use This Command.");
@@ -81,15 +81,13 @@ public class eram implements CommandExecutor {
         if (args.length == 0) {
             p.sendMessage(Translate.chat("&cUsage: Please use tab complete!"));
             return true;
-        } else if (args.length == 3 && args[0] != null && args[1] != null && args[2] != null && args[0].equalsIgnoreCase("save")) {
+        } else if (args.length == 3 && args[0] != null && args[1] != null && args[0].equalsIgnoreCase("save")) {
             String saveName = args[1].toLowerCase();
-            String letter = args[2].toUpperCase();
 
             aaa.computeIfAbsent(p.getDisplayName(), k -> new HashMap<>());
-            if (aaa.get(p.getDisplayName()).get(saveName) == null) {
-                aaa.get(p.getDisplayName()).put(saveName, new RawLocationObject());
-            }
-            aaa.get(p.getDisplayName()).get(saveName).set(letter, latestClickedBlocked.get(p.getDisplayName()));
+            aaa.get(p.getDisplayName()).computeIfAbsent(saveName, k -> new ArrayList<>());
+
+            aaa.get(p.getDisplayName()).get(saveName).add(latestClickedBlocked.get(p.getDisplayName()));
             eRamManager.saveThingy(p.getDisplayName(), p.getUniqueId(), saveName);
             return true;
         } else if (args.length == 1 && args[0].equalsIgnoreCase("load")) {
@@ -99,7 +97,14 @@ public class eram implements CommandExecutor {
             eRamManager.doIt(p.getDisplayName(), args[1]);
             return true;
         } else if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
+            if (aaa.get(p.getDisplayName()) == null) {
+                aaa.computeIfAbsent(p.getDisplayName(), k -> new HashMap<>());
+                p.sendMessage("You almost broke the plugin but this if statement saved it!");
+                return true;
+            }
+
             if (aaa.get(p.getDisplayName()).isEmpty()) {
+                p.sendMessage(Translate.chat("There is nothing?"));
                 return true;
             }
             Set<String> homeSet = aaa.get(p.getDisplayName()).keySet();
@@ -111,6 +116,12 @@ public class eram implements CommandExecutor {
         } else if (args.length == 2 && args[0].equalsIgnoreCase("delete") && args[1] != null) {
             eRamManager.delete(p.getDisplayName(), p.getUniqueId(), args[1].toLowerCase());
             p.sendMessage(Translate.chat("&cDone..."));
+            return true;
+        } else if (args.length == 3 && args[0].equalsIgnoreCase("addtag")) {
+            String tagName = args[1].toLowerCase();
+            String string = args[2].toLowerCase();
+            Tag.addTag(p.getDisplayName(), tagName, string);
+            p.sendMessage(Translate.chat("&6Added tag -> " + tagName + " " + string));
             return true;
         } else {
             p.sendMessage("Something messed up!");
