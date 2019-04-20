6b6f01ac8abefe54413eac68444c054efc23b01f
andrew121410 <andrewcherring@gmail.com>
2019-04-20 00:28:05 -0400

    Pussy my ballstack

--- src/main/java/World16/Events/AsyncPlayerChatEvent.java
+++ src/main/java/World16/Events/AsyncPlayerChatEvent.java
@@ -32,7 +32,8 @@ public class AsyncPlayerChatEvent implements Listener {
         this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
 
         adminList.add("andrew121410");
-        adminList.add("AlphaGibbon43");
+        adminList.add("Andrzej_Przybyla");
+//        adminList.add("AlphaGibbon43");
     }
 
     @EventHandler
