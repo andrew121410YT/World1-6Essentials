6c6a30b6f0d6201df11ad566cc665534b870d38a
andre <andrewcherring@gmail.com>
2019-07-26 01:32:56 -0400

    Save point

--- src/main/java/World16/test/test1.java
+++ src/main/java/World16/test/test1.java
@@ -3,10 +3,14 @@ package World16.test;
 import World16.Main.Main;
 import World16.Managers.CustomConfigManager;
 import World16.Utils.API;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class test1 implements CommandExecutor {
 
@@ -38,7 +42,33 @@ public class test1 implements CommandExecutor {
         }
 
         if (args.length == 0) {
-            //Something here.
+            Vector vector1 = p.getLocation().toVector();
+
+            Vector vector2 = new Vector(654, 70, -188);
+            Vector vector3 = new Vector(649, 73, -193);
+
+            boolean yesorno = vector1.isInAABB(Vector.getMinimum(vector2, vector3), Vector.getMaximum(vector2, vector3));
+            p.sendMessage("IN BOX: " + yesorno);
+
+            Location locVector2 = vector2.toLocation(p.getWorld());
+            Location locVector3 = vector3.toLocation(p.getWorld());
+            Location locTempA = locVector3.subtract(locVector2);
+
+            double tX = locTempA.getX();
+            double tY = locTempA.getY();
+            double tZ = locTempA.getZ();
+
+            for (double x = tX; x < tX; x++) {
+                for (double y = tY; y < tY; y++) {
+                    for (double z = tZ; z < tZ; z++) {
+                        Location locTempB = new Location(p.getWorld(), x, y, z);
+                        Location locTempC = locTempB.add(locVector3);
+                        Block block = p.getWorld().getBlockAt(locTempC);
+                        block.setType(Material.LAPIS_BLOCK);
+                        p.sendMessage(locTempC.toString());
+                    }
+                }
+            }
             return true;
         } else {
             //SOMETHING HERE
