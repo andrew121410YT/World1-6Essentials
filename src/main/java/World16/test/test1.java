4fdb7f8cbee198b64a24671f5789186a5d4e71ba
andre <andrewcherring@gmail.com>
2019-07-29 20:54:25 -0400

    Math

--- src/main/java/World16/test/test1.java
+++ src/main/java/World16/test/test1.java
@@ -3,23 +3,23 @@ package World16.test;
 import World16.Main.Main;
 import World16.Managers.CustomConfigManager;
 import World16.Utils.API;
+import World16.Utils.SimpleMath;
 import World16.Utils.Translate;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class test1 implements CommandExecutor {
 
     private Main plugin;
     private API api;
+    private SimpleMath simpleMath;
 
     private CustomConfigManager customConfigManager;
 
@@ -28,6 +28,7 @@ public class test1 implements CommandExecutor {
 
         this.customConfigManager = customConfigManager;
         this.api = new API(this.plugin);
+        this.simpleMath = new SimpleMath(this.plugin);
 
         this.plugin.getCommand("testee1").setExecutor(this);
     }
@@ -54,10 +55,10 @@ public class test1 implements CommandExecutor {
             Vector vector2 = loc2.toVector();
             Vector vector3 = loc3.toVector();
 
-            boolean yesorno = vector1.isInAABB(Vector.getMinimum(vector2, vector3), Vector.getMaximum(vector2, vector3));
+            boolean yesorno = simpleMath.isInAABBSimple(vector1, vector2, vector3);
             p.sendMessage("IN BOX: " + yesorno);
 
-            List<Location> locationsList = getHollowCube(loc2, loc3);
+            List<Location> locationsList = simpleMath.getHollowCubeNoOutline(loc2, loc3);
 
             for (Location location : locationsList) {
                 p.getWorld().getBlockAt(location).setType(Material.REDSTONE_BLOCK);
@@ -69,32 +70,5 @@ public class test1 implements CommandExecutor {
             //SOMETHING HERE
             return true;
         }
-    }
-
-    public List<Location> getHollowCube(Location corner1, Location corner2) {
-        List<Location> result = new ArrayList<Location>();
-        World world = corner1.getWorld();
-        double minX = Math.min(corner1.getX(), corner2.getX());
-        double minY = Math.min(corner1.getY(), corner2.getY());
-        double minZ = Math.min(corner1.getZ(), corner2.getZ());
-        double maxX = Math.max(corner1.getX(), corner2.getX());
-        double maxY = Math.max(corner1.getY(), corner2.getY());
-        double maxZ = Math.max(corner1.getZ(), corner2.getZ());
-
-        for (double x = minX; x <= maxX; x++) {
-            for (double y = minY; y <= maxY; y++) {
-                for (double z = minZ; z <= maxZ; z++) {
-                    int components = 0;
-                    if (x == minX || x == maxX) components++;
-                    if (y == minY || y == maxY) components++;
-                    if (z == minZ || z == maxZ) components++;
-                    if (components >= 2) {
-                        result.add(new Location(world, x, y, z));
-                    }
-                }
-            }
-        }
-
-        return result;
     }
 }
